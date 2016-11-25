862297bfa47e4de3b4f909ff34faf6d18200807e
Mikel Duffy <mduffy23@gmail.com>
2016-11-24 18:53:14 -0800

    Implements exponent

--- src/recursion.js
+++ src/recursion.js
@@ -97,6 +97,15 @@ var range = function(x, y) {
 // Example:  exponent(4,3);  // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if (exp === 0) {
+    return 1;
+  }
+  if (exp >= 1) {
+    return base * exponent(base, exp - 1);
+  }
+  if (exp <= -1) {
+    return exponent(base, exp + 1) / base;
+  }
 };
 
 // 8. Determine if a number is a power of two.
